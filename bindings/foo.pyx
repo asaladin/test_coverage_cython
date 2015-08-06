4006200620e919590344103c2dfdc85ce3e2a32e
Adrien Saladin <adrien.saladin@gmail.com>
2015-08-06 14:56:52 +0200

    expose Foo class to Python

--- bindings/foo.pyx
+++ bindings/foo.pyx
@@ -7,4 +7,12 @@ cdef extern from "foo.h":
         Foo()
         int one()        
     
-
+cdef class Foo:
+    cdef CppFoo * thisptr
+    def __cinit__(self):
+        self.thisptr = new CppFoo()
+    def __dealloc__(self):
+        if self.thisptr:
+           del self.thisptr
+    def one(self):
+        return self.thisptr.one()
